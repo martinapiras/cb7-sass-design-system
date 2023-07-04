e6a686e0a30a2ea0b67f6bd2905a94fb57b1114e
martinapiras <131855775+martinapiras@users.noreply.github.com>
2023-07-04 17:49:58 +0200

    04-07-2023

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,105 @@ import "./App.scss";
 function App() {
   return (
     <>
-      <h1>Title</h1>
+      <h1 className="heading-1">Title</h1>
       <div className="container">
-        <p>
+        <p className="col-4 col-xs-12">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis,
           aliquid.
         </p>
-        <p>
+        <p className="col-4 col-xs-12">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis eaque
           optio commodi impedit quas nisi fugiat vel aut adipisci eum.
         </p>
-        <p>
+        <p className="col-4 col-xs-12">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda
           eos, provident earum, optio beatae temporibus, aliquid explicabo
           reprehenderit nihil rem soluta molestiae excepturi doloribus corporis
           incidunt dolores. Nemo, fugit in.
         </p>
       </div>
-      <button className="success">
-        <span>Button</span>
-        <span>✔</span>
-      </button>
-      <button className="error">
-        <span>Button</span>
-        <span>❌</span>
-      </button>
-      <button className="secondary">
-        <span>Button</span>
-        <span>🐱‍👓</span>
-      </button>
+      <div className="buttons-row">
+        <button className="success">
+          <span>Button</span>
+          <span>✔</span>
+        </button>
+        <button className="error">
+          <span>Button</span>
+          <span>❌</span>
+        </button>
+        <button className="secondary">
+          <span>Button</span>
+          <span>🐱‍👓</span>
+        </button>
+        <button className="elevation">Elevation</button>
+      </div>
+      <div className="container">
+        <div className="row-g">
+          <div className="col-g-1">Col 1</div>
+          <div className="col-g-1">Col 1</div>
+          <div className="col-g-1">Col 1</div>
+          <div className="col-g-1">Col 1</div>
+          <div className="col-g-1">Col 1</div>
+          <div className="col-g-1">Col 1</div>
+          <div className="col-g-1">Col 1</div>
+          <div className="col-g-1">Col 1</div>
+          <div className="col-g-1">Col 1</div>
+          <div className="col-g-1">Col 1</div>
+          <div className="col-g-1">Col 1</div>
+          <div className="col-g-1">Col 1</div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-1">Col 1</div>
+          <div className="col-1">Col 1</div>
+          <div className="col-1">Col 1</div>
+          <div className="col-1">Col 1</div>
+          <div className="col-1">Col 1</div>
+          <div className="col-1">Col 1</div>
+          <div className="col-1">Col 1</div>
+          <div className="col-1">Col 1</div>
+          <div className="col-1">Col 1</div>
+          <div className="col-1">Col 1</div>
+          <div className="col-1">Col 1</div>
+          <div className="col-1">Col 1</div>
+          <div className="col-2">Col 2</div>
+          <div className="col-2">Col 2</div>
+          <div className="col-2">Col 2</div>
+          <div className="col-2">Col 2</div>
+          <div className="col-2">Col 2</div>
+          <div className="col-2">Col 2</div>
+          <div className="col-3 col-xs-12">
+            <img src="https://picsum.photos/350?1" alt="image" />
+          </div>
+          <div className="col-3 col-xs-12">
+            <img src="https://picsum.photos/350?2" alt="image" />
+          </div>
+          <div className="col-3 col-xs-12">
+            <img src="https://picsum.photos/350?3" alt="image" />
+          </div>
+          <div className="col-3 col-xs-12">
+            <img src="https://picsum.photos/350?4" alt="image" />
+          </div>
+          <div className="col-4">Col 4</div>
+          <div className="col-4">Col 4</div>
+          <div className="col-4">Col 4</div>
+          <div className="col-4 col-xs-12 keanu">
+            <img src="https://placekeanu.com/200/200" alt="keanu" />
+          </div>
+          <div className="col-8 col-xs-12">
+            <div className="col-2 col-xs-6">Col 2</div>
+            <div className="col-2 col-xs-6">Col 2</div>
+            <div className="col-2 col-xs-6">Col 2</div>
+            <div className="col-2 col-xs-6">Col 2</div>
+          </div>
+          <div className="col-5">Col 5</div>
+          <div className="col-7">Col 7</div>
+          <div className="col-6">Col 6</div>
+          <div className="col-6">Col 6</div>
+          <div className="col-12">Col 12</div>
+        </div>
+      </div>
     </>
   );
 }
